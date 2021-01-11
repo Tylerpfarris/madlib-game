ff86f0b21d03597b5cd47913053245bc753be447
Tyler Farris <Tylerpfarris@gmail.com>
2021-01-10 20:17:07 -0800

    as much as i could get done under the circumstances

--- app.js
+++ app.js
@@ -1,5 +1,53 @@
-// import functions and grab DOM elements
 
-// initialize state
 
-// set event listeners to update state and DOM
+// inputs
+
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+const input3 = document.getElementById('input3');
+const input4 = document.getElementById('input4');
+const input5 = document.getElementById('input5');
+const input6 = document.getElementById('input6');
+const input7 = document.getElementById('input7');
+const input8 = document.getElementById('input8');
+const input9 = document.getElementById('input9');
+const input10 = document.getElementById('input10');
+const input11 = document.getElementById('input11');
+const input12 = document.getElementById('input12');
+
+
+//spans
+
+const span1 = document.getElementById('span-1');
+const span2 = document.getElementById('span-2');
+const span3 = document.getElementById('span-3');
+const span4 = document.getElementById('span-4');
+const span5 = document.getElementById('span-5');
+const span6 = document.getElementById('span-6');
+const span7 = document.getElementById('span-7');
+const span8 = document.getElementById('span-8');
+const span9 = document.getElementById('span-9');
+const span10 = document.getElementById('span-10');
+const span11 = document.getElementById('span-11');
+const span12 = document.getElementById('span-12');
+
+const madLibButton = document.getElementById('mad-lib');
+madLibButton.addEventListener('click', () => {
+    spanEntry();
+});
+
+function spanEntry() {
+
+    const input1Text = input1.nodeValue;
+    span1.textContent = input1Text;
+}
+
+
+
+
+
+    
+
+
+
+
